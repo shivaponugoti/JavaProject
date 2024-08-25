25a4b5b350695b2020ae4f39cabd62308a44c05d
SivamaniCodes <129640630+SivamaniCodes@users.noreply.github.com>
2024-08-25 15:36:22 -0500

    Initial Commit

---
+++ HELLO/src/leetcode/jeff.java
@@ -0,0 +1,5 @@
+package leetcode;
+
+public interface jeff {
+
+}
---
+++ HELLO/src/leetcode/thomas.java
@@ -0,0 +1,9 @@
+package leetcode;
+
+public class thomas {
+
+	public static void main(String[] args) {
+		System.out.println("thomas jeffery");
+	}
+
+}
