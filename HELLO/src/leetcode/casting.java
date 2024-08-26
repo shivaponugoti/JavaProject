47a9b3ad98361bba3211c4ba06d55929adf190f1
Manager Siva <sivakoteswararao85@gmail.com>
2024-08-25 21:11:50 -0500

    int converted to float

--- HELLO/src/leetcode/casting.java
+++ HELLO/src/leetcode/casting.java
@@ -3,9 +3,9 @@ package leetcode;
 public class casting {
 
 	public static void main(String[] args) {
-		byte b =125;
 		int a = 260;
-		int c = (byte)a;
+		float f = 4.5f;
+		float c = (float)a;
 		System.out.println(c);
 	}
 
