134cfd9d6e05d5672abbad4f9540f14c9dd7fb1b
Manager Siva <sivakoteswararao85@gmail.com>
2024-08-25 21:09:36 -0500

    type casting and conversions

--- HELLO/src/datatypes/Number.java
+++ HELLO/src/datatypes/Number.java
@@ -2,12 +2,21 @@ package datatypes;
 
 public class Number {
 
-	public static void main(String[] args) {
-		int a = 7;
-		int b = 8;
-		int sum = a+b;
-		System.out.print(sum);
-		
+	public static void main(String[] args) 
+	{
+		//byte b = 123;
+		int a = 259;
+		//int c =  b;            byte converted to int
+		//double d=  8.8;
+		//double c = (int)a;     int  converted to double
+		//double c =(int)d;       double converted to int
+		//byte b = 12;
+		//int c = (byte)b;
+		float f = 2.5f;
+		//int c = (byte)a;    // int  converted to byte
+		//float c =(int)f;      //float converted to int
+		//float c = (float)a;   //int converted to float
+		System.out.println(c);
 	}
 
 }
